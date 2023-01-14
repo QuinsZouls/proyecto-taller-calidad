a5c5b32b052f27bf1efa4f6416ce9d2f356f9ad9
Alfredo Medrano <alfredomedranosanchez@gmail.com>
2023-01-13 23:18:15 -0600

    feature: add java test project

---
+++ test/test/src/test/java/com/test/TestNgTestBase.java
@@ -0,0 +1,46 @@
+package com.test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+
+import ru.stqa.selenium.factory.WebDriverFactory;
+
+import com.test.util.PropertyLoader;
+
+/**
+ * Base class for all the TestNG-based test classes
+ */
+public class TestNgTestBase {
+
+  protected WebDriver driver;
+  protected String webdriverPath;
+  protected String baseUrl;
+
+  @BeforeClass
+  public void init() throws IOException {
+    DesiredCapabilities dc = DesiredCapabilities.chrome();
+
+    baseUrl = PropertyLoader.loadProperty("site.url");
+    webdriverPath = PropertyLoader.loadProperty("browser.driver_path");
+
+    System.setProperty("webdriver.chrome.driver", webdriverPath);
+    driver = new ChromeDriver(dc);
+
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @AfterSuite(alwaysRun = true)
+  public void tearDown() {
+    if (driver != null) {
+      WebDriverFactory.dismissDriver(driver);
+      driver.quit();
+    }
+  }
+}
